d2181ae55bc7ff62e83b84419dc62cbc7e6ed40c
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-30 11:45:56 -0700

    add price of pizza and topping and displaying in html

--- Project4/js/scripts.js
+++ Project4/js/scripts.js
@@ -2,29 +2,29 @@
 var sIze;
 var extraTopping;
 
- function Pizza(size, toppings ) {
+ function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = toppings;
+  this.toppings = [];
   this.price = 0;
 };
-
 Pizza.prototype.totalPrice = function(price) {
   this.price += this.toppings;
-  return this.price;
-}
 
+}
 
 //User Interface
 
 $(document).ready(function() {
   $("#placeOrder").click(function(event) {
    event.preventDefault()
+
      sIze = $("#size").val();
     extraTopping = $("#topping").val();
 
 var newOrder = new Pizza(size);
-if(extraTopping === "Cheese") {
-  $("#totalprice").append("<li>" + extraTopping + "</li>");
+if(extraTopping === "Cheese" || sIze === "Large") {
+  this.price = 6;
+  $("#totalprice").text(sIze + " " + extraTopping + " " + "$" + this.price);
 }
 
 
