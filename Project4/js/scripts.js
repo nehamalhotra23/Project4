d51caff62a84cff562241fc40376e14d1f72eae1
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-30 13:19:50 -0700

    fix Nan error in returning price

--- Project4/js/scripts.js
+++ Project4/js/scripts.js
@@ -1,33 +1,34 @@
 //Backend Logic
 var sIze;
 var extraTopping;
+var newOrder = new Pizza();
 
- function Pizza(size, toppings) {
-  this.size = size;
-  this.toppings = [];
-  this.price = 0;
+function Pizza(size) {
+  this.size;
+  this.price = 5;
+  this.toppings = 1;
 };
 Pizza.prototype.totalPrice = function(price) {
-  this.price += this.toppings;
-
+    this.price += this.topping;
+     return this.price;
+     console.log(this.price);
 }
 
 //User Interface
 
 $(document).ready(function() {
   $("#placeOrder").click(function(event) {
-   event.preventDefault()
-
-     sIze = $("#size").val();
+    event.preventDefault()
+    sIze = $("#size").val();
     extraTopping = $("#topping").val();
+    console.log(extraTopping);
+     if(extraTopping === "Cheese" || size === "large"){
+       $("#totalprice").text(sIze + " " + extraTopping + " " + "$" + newOrder.totalPrice());
+  } if (extraTopping === "Pepperoni" || size === "large") {
+     $("#totalPrice").text(sIze + " " + extraTopping + " " + "$" + newOrder.totalPrice())
+  }
 
-var newOrder = new Pizza(size);
-if(extraTopping === "Cheese" || sIze === "Large") {
-  this.price = 6;
-  $("#totalprice").text(sIze + " " + extraTopping + " " + "$" + this.price);
-}
 
-
-});
+  });
 
 });
